2152ca155f5f197c61e87efd347a8b454e0dfdcf
german2717@gmail.com <german2717@gmail.com>
2023-04-18 11:46:24 -0300

    nose

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,14 +1,13 @@
 
-
 export default function Cards(props) {
-   const { character } = props;
+   // const { character } = props; // para no poner props.character.map
 
    return (
-      <div>
-      {character.map((char)=>{
+      <div className="App">
+      {props.character.map((char)=>{
          return(
        <div className='App' style={{ padding: '25px' }}>
-         {char.name} {char.gender} {char.species} {char.image}
+         {char.name} {char.gender} {char.species} {<img src={char.image}/>}
       </div>
          )
    })}
