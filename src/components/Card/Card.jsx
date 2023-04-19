439db33f8a00ff9762b988bf77ae6d510bc2be41
German Montenegro <112520841+montenegrog27@users.noreply.github.com>
2023-04-18 23:14:56 -0300

    j

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,10 +6,11 @@ export default function Card(props) {
       <div className={style.componente}>
          <div className={style.imagen}>
             { <img src={character.image} alt={""} /> }  
-            <h2 className={style.name}>Name: {character.name}</h2>
+
             { <button className={style.button} onClick ={onClose}  >X</button> }
          </div>
          <div className={style.atributes}>
+         <h2>Name: {character.name}</h2>
             <h2>Species:{character.species}</h2>
             <h2>Gender:{character.gender}</h2>
          </div>
