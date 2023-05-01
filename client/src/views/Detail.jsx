321e266fe09d426ab358c2335c86783307ad8b01
unknown <german2717@gmail.com>
2023-05-01 10:15:46 -0300

    tarjeta de rick

--- client/src/views/Detail.jsx
+++ client/src/views/Detail.jsx
@@ -1,70 +1,74 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useEffect } from "react";
-import style from './Detail.module.css'
+import style from "./Detail.module.css";
 
+function Detail() {
+  const [character, setCharacter] = useState([]);
 
+  const { id } = useParams(); // este numero viene en forma de string
 
-function Detail () {
-    const [character,setCharacter] = useState ([])
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/${id}`)
+      .then((response) => response.json())
+      .then((char) => {
+        if (char.name) {
+          setCharacter(char);
+        } else {
+          window.alert("No hay personajes con ese ID");
+        }
+      })
+      .catch((err) => {
+        window.alert("No hay personajes con ese ID");
+      });
+    return setCharacter({});
+  }, [id]);
 
-    const{id} = useParams()// este numero viene en forma de string
-
-
-
-    useEffect(() => {
-        fetch(`https://rickandmortyapi.com/api/character/${id}`)
-          .then((response) => response.json())
-          .then((char) => {
-            if (char.name) {
-              setCharacter(char);
-            } else {
-              window.alert("No hay personajes con ese ID");
-            }
-          })
-          .catch((err) => {
-            window.alert("No hay personajes con ese ID");
-          });
-        return setCharacter({});
-      }, [id]);
-
-
-
-    return(
-        <>
-        <div className={style.nav}>
-        <Link to='/home' className={style.home}>Home</Link>
-        <Link to='/about' className={style.about}>About</Link>
-        </div>
-        <div className={style.cardConteiner}>
-            <div className={style.name}>
+  return (
+    <>
+      <div className={style.todo2}>
+        <div className={style.todo1}>
+          <div className={style.todo}>
+            <div className={style.nav}>
+              <Link to="/home" className={style.home}>
+                Home
+              </Link>
+              <Link to="/about" className={style.about}>
+                About
+              </Link>
+            </div>
+            <div className={style.cardConteiner}>
+              <div className={style.name}>
                 <h3>Name</h3>
                 {character.name}
-            </div>
-            <div className={style.status}>
+              </div>
+              <div className={style.status}>
                 <h3>Status</h3>
                 {character.status}
-            </div>
-            <div className={style.specie}>
+              </div>
+              <div className={style.specie}>
                 <h3>Specie</h3>
                 {character.specie}
-            </div>
-            <div className={style.gender}>
+              </div>
+              <div className={style.gender}>
                 <h3>Gender</h3>
                 {character.gender}
-            </div>
-            <div className={style.origin}>
+              </div>
+              <div className={style.origin}>
                 <h3>Origin</h3>
                 {character.origin?.name}
+              </div>
             </div>
-        </div>
-        <div className={style.imageConteiner1}>
-            <div className={style.imageconteiner2}>
+            <div className={style.imageConteiner1}>
+              <div className={style.imageconteiner2}>
                 <img src={character.image} alt="" className={style.imagen} />
+              </div>
             </div>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Detail;
